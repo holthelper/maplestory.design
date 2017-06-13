71a673a20321845d5c5b28be9b923f207824207f
Tyler Corsair <tyler@tylercorsair.com>
2017-06-13 04:23:16 -0700

    Further Restyling

--- src/components/CharacterProperties/index.js
+++ src/components/CharacterProperties/index.js
@@ -6,19 +6,22 @@ class CharacterProperties extends Component {
   render() {
     return (
       <div className='character-properties'>
-        <span>Character Properties</span>
+        <span>Properties</span>
         <div className="facial-expression">
           <span>Facial Expression</span>
-          <select>
+          <select disabled>
             <option>Default</option>
           </select>
         </div>
         <div className="action">
           <span>Pose / Action</span>
-          <select>
-            <option>Stand1</option>
-            <option>Stand2</option>
+          <select disabled>
+            <option>Standing</option>
+            <option>Standing 2</option>
           </select>
+        </div>
+        <div className="disclaimer">
+          This is currently a prototype and a <b>work in progress</b>. Please provide feedback or report bugs on our <a href="https://discord.gg/D65Grk9">Discord</a>.
         </div>
       </div>
     )
